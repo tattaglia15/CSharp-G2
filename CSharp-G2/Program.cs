1010dd5e599632d55886f81824d9a8a2b48af0de
tattaglia15 <stephane_lik@hotmail.com>
2022-03-08 20:34:06 +0100

    Add project files.

---
+++ CSharp-G2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp_G2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   *");
+            Console.WriteLine("   *");
+            Console.WriteLine(" * * *");
+            Console.WriteLine("*  *  *");
+            Console.WriteLine("   *");
+            Console.WriteLine("   *");
+            Console.WriteLine(" *   *");
+            Console.WriteLine("*     *");
+            Console.WriteLine("Here is some text");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+
+        }
+    }
+}
