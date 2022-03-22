ddc87791ba15611aee5d72f76471d9356ed69eff
tattaglia15 <96620459+tattaglia15@users.noreply.github.com>
2022-03-22 14:42:18 +0100

    Add files via upload

---
+++ Class - 03 -HomeWork- SumOfEvenNums/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNums = new int[6];
+            int sum = 0;
+            for (int i = 0; i <arrayOfNums.Length; i++)
+            {
+                Console.WriteLine("Enter Number:");
+                string numbers = Console.ReadLine();
+                bool ifNumberParsed = int.TryParse(numbers, out int parsedNumber);
+                if(!ifNumberParsed || parsedNumber <= 0)
+                {
+                    Console.WriteLine("Invalid Input");
+                    break;
+                }
+                if(parsedNumber % 2 == 0)
+                {
+                    sum += parsedNumber;
+                }
+            }
+            Console.WriteLine($"The sum of all even numbers is {sum}");
+            
+
+
+        }
+    }
+}
---
+++ Class-03-HomeWork-StudentGroup/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] studentsGoOne = { "Zdravko", "Bojan", "Petko", "Milosh", "Kristijan" };
+            string[] studentsGtTwo = { "Bojana", "Kristijan", "Tanja", "Marko", "Damjan" };
+            Console.WriteLine("Please Enter Student Group from 1 - 2:");
+            string number = Console.ReadLine();
+            bool ifisValideNumber = int.TryParse(number, out int parsedNumber);
+            if (!ifisValideNumber || parsedNumber > 2 || parsedNumber <= 0)
+            {
+                Console.WriteLine("Wrong Input");
+            }
+            if(parsedNumber == 1)
+            {
+                foreach (string student in studentsGoOne)
+                {
+                    Console.WriteLine(student);
+                }
+            } else if(parsedNumber == 2)
+            {
+                foreach(string secStudent in studentsGtTwo)
+                {
+                    Console.WriteLine(secStudent);
+                }
+            }
+            
+            
+        }
+    }
+}
