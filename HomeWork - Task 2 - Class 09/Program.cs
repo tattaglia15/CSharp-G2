cbddf13939a60abfae37605f210f5c510ce822bb
tattaglia15 <96620459+tattaglia15@users.noreply.github.com>
2022-04-04 15:49:35 +0200

    Add files via upload

---
+++ HomeWork - Task 2 - Class 09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeWork___Task_2___Class_09
+{
+    internal class Program
+    {
+        private static int num;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List of 10 numbers");
+            List<int> listOfNumbers = new List<int>()
+            {
+                {15},
+                {17},
+                {2},
+                {4},
+                {5},
+                {6},
+                {7},
+                {8},
+                {9},
+                {10},
+            };
+            List<int> squareNums = listOfNumbers.Select(num => num * 2).ToList();
+            foreach(int num in squareNums)
+            {
+                Console.WriteLine($"The Square Of Numbers in List:{num}");
+            }
+        }
+    }
+}
